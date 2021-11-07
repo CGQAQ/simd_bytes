1f640ca5b59af7e94bfc0d4518da5790eb37cb39
CGQAQ <m.jason.liu@outlook.com>
2021-11-07 22:59:56 +0800

    x86 simd types
    Signed-off-by: CGQAQ <m.jason.liu@outlook.com>

--- src/vendor/x86.rs
+++ src/vendor/x86.rs
@@ -4,6 +4,25 @@ use core::arch::x86::*;
 #[cfg(target_arch = "x86_64")]
 use core::arch::x86_64::*;
 
-union SimdX86Layout128 {
-    mm: __m128i,
+macro_rules! simd_layout {
+    ($name: ident, $ty: ty, $num: literal) => {
+        union $name {
+            mm: $ty,
+            m8: [u8; $num / 8],
+            m8i: [i8; $num / 8],
+            m16: [u16; $num / 16],
+            m16i: [i16; $num / 16],
+            m32: [u32; $num / 32],
+            m32i: [i32; $num / 32],
+            m64: [u64; $num / 64],
+            m64i: [i64; $num / 64],
+        }
+    };
 }
+
+simd_layout!(SimdX86Layout128, __m128i, 128);
+
+simd_layout!(SimdX86Layout256, __m256i, 256);
+
+// TODO(CGQAQ): Uncomment this line to enable 512bit SIMD as soon as it becomes stable.
+// simd_layout!(SimdX86Layout512, __m512i, 128);
